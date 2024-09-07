6db24244f084634d05000f900bd8435fb09f46e5
SauravGaur1 <sauravgaur@valueresearch.in>
2024-09-08 01:19:49 +0530

    added long polling

---
+++ long-polling/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+const jobs = {}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/create', (req, res) => {
+    const jobId = `task:${Date.now()}`;
+    jobs[jobId] = 0; //progress
+    someHeavyOperationSimulation(jobId);
+    res.end(`Job Created : ${jobId}`);
+})
+
+app.get('/status', async (req, res) => {
+    const { jobId } = req.query;
+    if(!!!jobId) {
+        res.end(`No Job with job id ${jobId} exists`);
+    }
+    while(await checkStatus(jobId) == false);
+    const progress = jobs[jobId];
+
+    res.end(`Status: ${progress}%`);
+})
+
+async function checkStatus(jobId) {
+    return new Promise((resolve, reject) => {
+        if(jobs[jobId] < 100) {
+            setTimeout(() => resolve(false), 100);
+        } else {
+            resolve(true);
+        }
+    })
+}
+
+function someHeavyOperationSimulation(jobId) {
+    let progress = jobs[jobId];
+    if(progress == 100) return;
+    setTimeout(() => {
+        jobs[jobId] += 10;
+        someHeavyOperationSimulation(jobId);
+    }, 500);
+}
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+})
